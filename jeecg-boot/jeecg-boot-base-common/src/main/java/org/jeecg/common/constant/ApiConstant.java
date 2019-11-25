f8625656c1c92516266d35a1bbaf5bf33127aecb
gaspplus <275132103@qq.com>
2019-11-25 13:50:33 +0800

    接口常量

--- jeecg-boot/jeecg-boot-base-common/src/main/java/org/jeecg/common/constant/ApiConstant.java
+++ jeecg-boot/jeecg-boot-base-common/src/main/java/org/jeecg/common/constant/ApiConstant.java
@@ -3,7 +3,7 @@ package org.jeecg.common.constant;
 /**
  * api常量
  */
-public class ApiConstant {
+public interface ApiConstant {
     public static final String VERSION = "v1";//api请求路径版本
     public static final String LOGIN_USER_KEY = "LOGIN_USER_KEY";//会员登陆request存储key
 }
