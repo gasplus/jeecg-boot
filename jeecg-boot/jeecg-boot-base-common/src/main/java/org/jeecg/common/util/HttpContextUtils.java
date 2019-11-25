0c7d67926503e59ce5a8d7baa8dacc867a189883
gaspplus <275132103@qq.com>
2019-11-25 13:56:58 +0800

    增加获取request方法

---
+++ jeecg-boot/jeecg-boot-base-common/src/main/java/org/jeecg/common/util/HttpContextUtils.java
@@ -0,0 +1,15 @@
+package org.jeecg.common.util;
+
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 获取reqeust
+ */
+public class HttpContextUtils {
+    public static HttpServletRequest getHttpServletRequest() {
+        return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+    }
+}
